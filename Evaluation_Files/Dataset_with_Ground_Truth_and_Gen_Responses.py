35b1e562e63434bf5ffe85c70c27438a916fde98
Osato Ogunbor <154603931+Osatoogunbor@users.noreply.github.com>
2025-02-27 22:34:12 +0000

    Update Dataset_with_Ground_Truth_and_Gen_Responses.py

--- Evaluation_Files/Dataset_with_Ground_Truth_and_Gen_Responses.py
+++ Evaluation_Files/Dataset_with_Ground_Truth_and_Gen_Responses.py
@@ -9,8 +9,8 @@ import os
 nest_asyncio.apply()
 
 # Load API Keys
-OPENAI_API_KEY = "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"
-PINECONE_API_KEY = "pcsk_4MmQQE_QVvEdpRKLtekQvxKHp9P5QUEWZe7NMzRvdZzXXheWoxTEkTYFYTqYQCYmUop3RP"
+OPENAI_API_KEY = "openai_api_key"
+PINECONE_API_KEY = "pincone_api_key"
 
 # Initialize OpenAI and Pinecone clients
 client = AsyncOpenAI(api_key=OPENAI_API_KEY)
